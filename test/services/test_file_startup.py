3c16a3e823bf3a3181b07ebc71c48a28479d27ac
Elias Mueller <elias.mr1@gmail.com>
2024-10-06 22:11:06 +0200

    Refactor file startup service

--- test/services/test_file_startup.py
+++ test/services/test_file_startup.py
@@ -15,56 +15,68 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
 from unittest.mock import MagicMock
 
 import inject
-from parameterized import parameterized
+import pytest
 
 from mpvqc.services import ApplicationPathsService, FileStartupService, ResourceService
 
 
-class FileStartupServiceTest(unittest.TestCase):
-    MODULE = "mpvqc.services.file_startup"
+@pytest.fixture()
+def file_startup_service():
+    return FileStartupService()
 
-    mocked_file_service = MagicMock()
 
-    def setUp(self):
-        # fmt: off
-        inject.clear_and_configure(lambda binder: binder
-                                   .bind(ApplicationPathsService, self.mocked_file_service)
-                                   .bind(ResourceService, MagicMock()))
-        # fmt: on
+@pytest.fixture()
+def application_paths_service_mock():
+    return MagicMock()
 
-    def tearDown(self):
-        inject.clear()
 
-    @parameterized.expand(
-        [
-            "dir_config",
-            "dir_backup",
-            "dir_screenshots",
-            "dir_export_templates",
-        ]
-    )
-    def test_directories_created(self, mocked_dir):
-        service = FileStartupService()
-        service.create_missing_directories()
-        path_mock = getattr(self.mocked_file_service, mocked_dir)
-        path_mock.mkdir.assert_called()
+@pytest.fixture(autouse=True)
+def configure_inject(application_paths_service_mock):
+    def config(binder: inject.Binder):
+        binder.bind(ApplicationPathsService, application_paths_service_mock)
+        binder.bind(ResourceService, MagicMock())
 
-    @parameterized.expand(
-        [
-            "file_input_conf",
-            "file_mpv_conf",
-        ]
-    )
-    def test_files_created(self, mocked_file):
-        path_mock = getattr(self.mocked_file_service, mocked_file)
-        path_mock.exists.return_value = False
+    inject.configure(config, clear=True)
 
-        service = FileStartupService()
-        service.create_missing_files()
 
-        path_mock.exists.assert_called()
-        path_mock.write_text.assert_called()
+@pytest.mark.parametrize(
+    "mocked_dir",
+    [
+        "dir_config",
+        "dir_backup",
+        "dir_screenshots",
+        "dir_export_templates",
+    ],
+)
+def test_directories_created(
+    application_paths_service_mock,
+    file_startup_service,
+    mocked_dir,
+):
+    file_startup_service.create_missing_directories()
+    path_mock = getattr(application_paths_service_mock, mocked_dir)
+    path_mock.mkdir.assert_called()
+
+
+@pytest.mark.parametrize(
+    "mocked_file",
+    [
+        "file_input_conf",
+        "file_mpv_conf",
+    ],
+)
+def test_files_created(
+    application_paths_service_mock,
+    file_startup_service,
+    mocked_file,
+):
+    path_mock = getattr(application_paths_service_mock, mocked_file)
+    path_mock.exists.return_value = False
+
+    file_startup_service.create_missing_files()
+
+    path_mock.exists.assert_called()
+    path_mock.write_text.assert_called()
