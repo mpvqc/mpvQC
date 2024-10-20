7529ee10ba2db5548596fca52e420e7ee0fdcd07
Elias Mueller <elias.mr1@gmail.com>
2024-10-20 19:09:16 +0200

    Add combi test

--- test/pyobjects/comment_model/conftest.py
+++ test/pyobjects/comment_model/conftest.py
@@ -71,3 +71,31 @@ def make_model() -> Callable[[Iterable[Comment], int | float], MpvqcCommentModel
         return model
 
     return _make_model
+
+
+@pytest.fixture(scope="session")
+def make_sophisticated_model() -> (
+    Callable[[Iterable[Comment], int | float], tuple[MpvqcCommentModelPyObject, Callable[[int], None]]]
+):
+    def _make_model(
+        set_comments: Iterable[Comment],
+        set_player_time: int | float = 0,
+    ):
+        # noinspection PyCallingNonCallable
+        model: MpvqcCommentModelPyObject = MpvqcCommentModelPyObject()
+        model.import_comments(list(set_comments))
+
+        player_mock = MagicMock()
+        player_mock.current_time = set_player_time
+
+        def set_time(value: int):
+            player_mock.current_time = value
+
+        def config(binder: inject.Binder):
+            binder.bind(PlayerService, player_mock)
+
+        inject.configure(config, clear=True)
+
+        return model, set_time
+
+    return _make_model
