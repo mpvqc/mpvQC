bd5f9070ecff0f774d2688df2b30580e44b55a5c
Elias Mueller <elias.mr1@gmail.com>
2025-02-08 13:44:15 +0100

    feat(mpv.conv): use os specific default configs

--- test/services/test_resource.py
+++ test/services/test_resource.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from unittest.mock import MagicMock, patch
+
 import inject
 import pytest
 
@@ -36,7 +38,6 @@ def resource_service() -> ResourceService:
 
 def test_resources(resource_service):
     assert resource_service.input_conf_content
-    assert resource_service.mpv_conf_content
     assert resource_service.backup_template
     assert resource_service.default_export_template
 
@@ -53,3 +54,16 @@ def test_resources(resource_service):
     assert "comment['time'] | as_time" in export_template
     assert "comment['commentType'] | as_comment_type" in export_template
     assert "comment['comment'] | trim" in export_template
+
+
+def test_os_specific_resources(resource_service):
+    with patch("platform.system", MagicMock(return_value="Windows")):
+        assert resource_service.mpv_conf_content
+        assert "vo=gpu-next" in resource_service.mpv_conf_content.splitlines()
+
+    other_oses = ["Linux", "Darwin"]
+
+    for os in other_oses:
+        with patch("platform.system", MagicMock(return_value=os)):
+            assert resource_service.mpv_conf_content
+            assert not any(line.startswith("vo=") for line in resource_service.mpv_conf_content.splitlines())
