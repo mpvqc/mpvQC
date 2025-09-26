3a2e0b12cecf2a3fe58ce724e06c1e76087b5a61
Elias Mueller <elias.mr1@gmail.com>
2025-09-26 18:55:56 +0200

    refactor: apply mvc pattern to import settings dialog

--- mpvqc/controllers/__init__.py
+++ mpvqc/controllers/__init__.py
@@ -9,3 +9,4 @@ from .comment_types_dialog import MpvqcCommentTypesDialogControllerPyObject
 from .edit_input_dialog import MpvqcEditInputDialogControllerPyObject
 from .edit_mpv_dialog import MpvqcEditMpvDialogControllerPyObject
 from .export_settings_dialog import MpvqcExportSettingsDialogControllerPyObject
+from .import_settings_dialog import MpvqcImportSettingsDialogControllerPyObject
