759a8834d7a2d950b1f16c5401d9df826143b325
Elias Mueller <elias.mr1@gmail.com>
2025-09-27 18:54:17 +0200

    chore: remove obsolete search from model

--- test/models/comments/test_import.py
+++ test/models/comments/test_import.py
@@ -61,14 +61,6 @@ def test_import_sorts_comments(make_model):
     assert [c["comment"] for c in model.comments()] == ["Word 1", "Word 3", "Word 2", "Word 4"]
 
 
-def test_import_comments_invalidates_search_results(model):
-    model._searcher._hits = ["result"]
-
-    model.import_comments(DEFAULT_COMMENTS)
-
-    assert model._searcher._hits is None
-
-
 def test_import_comments_fires_signals(model, make_spy):
     initially_spy = make_spy(model.commentsImportedInitially)
 
@@ -107,17 +99,17 @@ def test_import_comments_undo_redo(model):
     assert comments[5]["comment"] == "Undo Redo 3"
 
 
-def test_import_comments_undo_redo_invalidates_search(model):
-    comment = Comment(time=99, comment_type="commentType", comment="Word 1")
-    model.import_comments([comment])
+def test_import_comments_undo_redo_invalidates_search(model, make_spy):
+    spy = make_spy(model.searchInvalidated)
 
-    model._searcher._hits = ["result"]
+    model.import_comments([(Comment(time=99, comment_type="commentType", comment="Word 1"))])
+    assert spy.count() == 1
+
     model.undo()
-    assert model._searcher._hits is None
+    assert spy.count() == 2
 
-    model._searcher._hits = ["result"]
     model.redo()
-    assert model._searcher._hits is None
+    assert spy.count() == 3
 
 
 def test_import_comments_undo_redo_fires_signals(model, make_spy):
