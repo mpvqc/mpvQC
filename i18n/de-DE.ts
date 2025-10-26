fb5b6cb1dd5404a1e8727285e404c73a9f2796da
Elias Mueller <elias.mr1@gmail.com>
2025-10-26 16:56:56 +0100

    chore: update translations

--- i18n/de-DE.ts
+++ i18n/de-DE.ts
@@ -254,6 +254,49 @@
     </message>
 </context>
 <context>
+    <name>ImportConfirmationDialog</name>
+    <message>
+        <source>Confirm Import</source>
+        <extracomment>Dialog title for confirming which videos and subtitles to import</extracomment>
+        <translation>Import bestätigen</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <extracomment>Section header for video selection list</extracomment>
+        <translation>Videos</translation>
+    </message>
+    <message>
+        <source>Skip video</source>
+        <extracomment>Option to import without selecting any video</extracomment>
+        <translation>Kein Video importieren</translation>
+    </message>
+    <message>
+        <source>From QC document</source>
+        <extracomment>Tooltip indicating the video file was referenced in one of the imported documents</extracomment>
+        <translation>Aus QC-Dokument</translation>
+    </message>
+    <message>
+        <source>From subtitle</source>
+        <extracomment>Tooltip indicating the video file was referenced in one of the imported subtitles</extracomment>
+        <translation>Aus Untertitel</translation>
+    </message>
+    <message>
+        <source>Subtitles</source>
+        <extracomment>Section header for subtitle selection list</extracomment>
+        <translation>Untertitel</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <extracomment>Button to select all subtitles in the list</extracomment>
+        <translation>Alle auswählen</translation>
+    </message>
+    <message>
+        <source>Deselect All</source>
+        <extracomment>Button to deselect all subtitles in the list</extracomment>
+        <translation>Alle abwählen</translation>
+    </message>
+</context>
+<context>
     <name>ImportSettingsDialog</name>
     <message>
         <source>Import Settings</source>
