a78d48fc868e72e6bc13ce5f2374927a3d2b2ce5
Elias Mueller <elias.mr1@gmail.com>
2025-11-01 17:57:40 +0100

    perf(win): use QWindow instead of QQuickWindow

--- mpvqc/views/player_win_id.py
+++ mpvqc/views/player_win_id.py
@@ -3,8 +3,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import inject
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QWindow
 from PySide6.QtQml import QmlElement
-from PySide6.QtQuick import QQuickWindow
 
 from mpvqc.services import FramelessWindowService, PlayerService
 
@@ -14,12 +15,17 @@ QML_IMPORT_MAJOR_VERSION = 1
 
 # noinspection PyUnresolvedReferences
 @QmlElement
-class MpvWindowPyObject(QQuickWindow):
+class MpvWindowPyObject(QWindow):
     _player: PlayerService = inject.attr(PlayerService)
     _frameless_window: FramelessWindowService = inject.attr(FramelessWindowService)
 
     def __init__(self):
         super().__init__()
+        self.setFlags(
+            Qt.WindowType.FramelessWindowHint
+            | Qt.WindowType.WindowDoesNotAcceptFocus
+            | Qt.WindowType.WindowTransparentForInput
+        )
         win_id = self.winId()
         self._player.init(win_id=win_id)
         self._frameless_window.set_embedded_player_hwnd(win_id)
