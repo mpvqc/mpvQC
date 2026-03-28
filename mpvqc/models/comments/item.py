6afcc6bc2d41725d4b95e5a35094ab503ef40335
Elias Mueller <elias.mr1@gmail.com>
2026-03-28 12:10:37 +0100

    fix: implement all comparison methods

--- mpvqc/models/comments/item.py
+++ mpvqc/models/comments/item.py
@@ -17,15 +17,47 @@ class CommentItem(QStandardItem):
         super().__init__()
         self._id = next(ID_COUNTER)
 
-    def __lt__(self, other: QStandardItem) -> bool:
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, CommentItem):
-            return False
+            return NotImplemented
+        return self._id == other._id
 
-        this_time = self.data(Role.TIME)
-        that_time = other.data(Role.TIME)
+    def __ne__(self, other: object) -> bool:
+        if not isinstance(other, CommentItem):
+            return NotImplemented
+        return self._id != other._id
 
-        if this_time < that_time:
-            return True
-        if this_time > that_time:
-            return False
+    def __hash__(self):
+        return super().__hash__()
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, CommentItem):
+            return NotImplemented
+        t1, t2 = self.data(Role.TIME), other.data(Role.TIME)
+        if t1 != t2:
+            return t1 < t2
         return self._id < other._id
+
+    def __le__(self, other: object) -> bool:
+        if not isinstance(other, CommentItem):
+            return NotImplemented
+        t1, t2 = self.data(Role.TIME), other.data(Role.TIME)
+        if t1 != t2:
+            return t1 < t2
+        return self._id <= other._id
+
+    def __gt__(self, other: object) -> bool:
+        if not isinstance(other, CommentItem):
+            return NotImplemented
+        t1, t2 = self.data(Role.TIME), other.data(Role.TIME)
+        if t1 != t2:
+            return t1 > t2
+        return self._id > other._id
+
+    def __ge__(self, other: object) -> bool:
+        if not isinstance(other, CommentItem):
+            return NotImplemented
+        t1, t2 = self.data(Role.TIME), other.data(Role.TIME)
+        if t1 != t2:
+            return t1 > t2
+        return self._id >= other._id
